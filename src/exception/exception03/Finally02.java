2509ab116182da2d42a15106330d368722632f92
yuri-candido <yuri.gcandido@hotmail.com>
2024-06-11 15:08:05 -0300

    using finally

---
+++ src/exception/exception03/Finally02.java
@@ -0,0 +1,25 @@
+package src.exception.exception03;
+
+public class Finally02 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {4, 8, 16, 32, 64, 128};
+        int[] denominators = {2, 0, 4, 8, 0};
+
+        for (int i = 0; i < numbers.length; i++) {
+            try {
+                System.out.println(numbers[i] + "/" + denominators[i] + " = " + (numbers[i] / denominators[i]));
+            } catch (ArithmeticException e) {
+                System.out.println("Error dividing by zero");
+                System.exit(0);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Invalid array position");
+                System.exit(0);
+            } finally {
+                System.out.println("This line is always printed after the try or catch");
+                System.out.println();
+            }
+        }
+    }
+}
