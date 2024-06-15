491b08d4fc29ee7db3f4e00ee5ec55c2979c070d
Yuri Gomes <yuri.gcandido@hotmail.com>
2024-06-15 18:34:05 -0300

    using interface

---
+++ src/interfaces/interface01/model/services/BrazilTaxService.java
@@ -0,0 +1,13 @@
+package src.interfaces.interface01.model.services;
+
+public class BrazilTaxService implements TaxService {
+
+    public double tax(double amount) {
+        if (amount <= 100.0) {
+            return amount * 0.2;
+        }
+        else {
+            return amount * 0.15;
+        }
+    }
+}
---
+++ src/interfaces/interface01/model/services/TaxService.java
@@ -0,0 +1,6 @@
+package src.interfaces.interface01.model.services;
+
+public interface TaxService {
+
+    double tax(double amount);
+}
